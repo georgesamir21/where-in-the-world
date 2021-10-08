7b74bcbf69c6d96c70264b13fc0594e5f432588d
georgesamir21 <georges.samir21@gmail.com>
2021-10-08 19:13:37 +0200

    create country card component

---
+++ src/components/CountryCard/index.tsx
@@ -0,0 +1,44 @@
+import './style.scss';
+
+type Props = {
+  flag?: string;
+  name?: string;
+  region?: string;
+  capital?: string;
+  population?: number;
+};
+
+export const CountryCard = ({
+  flag,
+  name,
+  region,
+  capital,
+  population,
+}: Props) => {
+  return (
+    <>
+      <div className="card">
+        <div className="card__image">
+          <img src={flag} alt={`${name} flag`} />
+        </div>
+        <div className="card__content">
+          <h3 className="title">{name}</h3>
+          <ul className="details">
+            <li>
+              <span className="bold">Population:</span>
+              {population}
+            </li>
+            <li>
+              <span className="bold">Region:</span>
+              {region}
+            </li>
+            <li>
+              <span className="bold">Capital:</span>
+              {capital}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+};
