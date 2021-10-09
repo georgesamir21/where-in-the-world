5ed086744facea512858b0a98e8c6ab28749d7e7
georgesamir21 <georges.samir21@gmail.com>
2021-10-09 15:31:52 +0200

    add search for country and filter by region inputs

--- src/components/CountriesList.tsx/index.tsx
+++ src/components/CountriesList.tsx/index.tsx
@@ -1,15 +1,58 @@
+import { ChangeEvent } from 'react';
 import { useGetCountries } from '../../hooks/useGetCountries';
 import { CountryCard } from '../CountryCard';
 import './style.scss';
 
 export const CountriesList = () => {
   const countries = useGetCountries();
-  if (!countries) return <h3>lOADING ....</h3>;
   console.log(countries);
+  const regions = [
+    {
+      label: 'Africa',
+      value: 'africa',
+    },
+    {
+      label: 'America',
+      value: 'america',
+    },
+    {
+      label: 'Asia',
+      value: 'asia',
+    },
+    {
+      label: 'Europe',
+      value: 'europe',
+    },
+    {
+      label: 'Ocenia',
+      value: 'ocenia',
+    },
+  ];
+  const handleCountriesSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    // TODO: Use Debounce...
+    const countryName = e.target.value;
+    console.log(countryName);
+  };
 
+  if (!countries) return <h3>Loading ....</h3>;
   return (
     <>
-      <div className="countries-cards">
+      <div>
+        <input
+          type="text"
+          name="country"
+          placeholder="Search for a country..."
+          onChange={(e) => handleCountriesSearch(e)}
+        />
+        <select name="region" placeholder="Filter by Region">
+          {regions.map(({ label, value }) => (
+            <option key={value} value={value}>
+              {label}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="countries d-flex-row">
         {countries.map((c) => (
           <CountryCard key={c.name} {...c} />
         ))}
